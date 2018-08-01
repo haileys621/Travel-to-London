cb0733df3f642ee0b5354f7b74b447e2f8a72fd8
haileys621 <42008480+haileys621@users.noreply.github.com>
2018-08-01 14:25:37 -0400

    Add files via upload
    My first commit

---
+++ travel_page.js
@@ -0,0 +1,37 @@
+  var index = 1;
+function changeImage1() {
+  if (index == 1) {
+    document.getElementById("bigBen").src="https://s1.r29static.com//bin/entry/ceb/0,461,2000,1050/x/1834513/image.png";
+    index += 1;
+  } else {
+    document.getElementById("bigBen").src="https://i.ytimg.com/vi/Qad6Q_RbQQ0/maxresdefault.jpg";
+    index -= 1;
+  }
+}
+function changeImage2() {
+  if (index == 1) {
+    document.getElementById("londonEye").src="https://cdn.touracti.com/touracti/img/product_images/product_standard_images/London%20eye.jpg";
+    index += 1;
+  } else {
+    document.getElementById("londonEye").src="https://cdn.londonandpartners.com/asset/20adda9d08e8480c6dbbfcf30fbcabdb.jpg";
+    index -= 1;
+  }
+}
+function changeImage3() {
+  if (index == 1) {
+    document.getElementById("buckPalace").src="https://cdn.londonandpartners.com/asset/changing-the-guard_changing-the-guard-at-buckingham-palace_463f8d67704f63af74b5b825d086cce8.jpg";
+    index += 1;
+  } else {
+    document.getElementById("buckPalace").src="https://cdn.getyourguide.com/img/tour_img-1260045-148.jpg";
+    index -= 1;
+  }
+}
+function changeImage4() {
+  if (index == 1) {
+    document.getElementById("shakeGlobe").src="https://www.daysoutguide.co.uk/media/431763/shakespeares-globe-1.jpg?anchor=center&mode=crop&width=618&height=350&rnd=131768136540000000";
+    index += 1;
+  } else {
+    document.getElementById("shakeGlobe").src="https://saichintala.files.wordpress.com/2014/07/ce169649_942long.jpg?w=942&h=942&crop=1";
+    index -= 1;
+  }
+}
